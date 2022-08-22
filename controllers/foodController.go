8037a39ba969a7d2598b6c6a131acc975f8ef9d6
joy-adhikary <joyadhikary074@gmail.com>
2022-08-22 23:44:05 +0600

    1.1.1

--- controllers/foodController.go
+++ controllers/foodController.go
@@ -1,10 +1,31 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"github.com/joy-adhikary/Restaurant-Management-back_end/database"
+	"github.com/joy-adhikary/Restaurant-Management-back_end/models"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var foodcollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+var validate = validator.New()
+
+//var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetFoods() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
+		var _, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		c.JSON(http.StatusOK, foodcollection)
 
 	}
 }
@@ -12,7 +33,18 @@ func GetFoods() gin.HandlerFunc {
 func GetFood() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
 
+		foodId := c.Param("food_id")
+
+		var food models.Food
+
+		err := foodcollection.FindOne(ctx, bson.M{"food_id": foodId}).Decode(&food)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error in fatching data "})
+		}
+		c.JSON(http.StatusOK, food)
 	}
 }
 
@@ -24,7 +56,45 @@ func UpdateFood() gin.HandlerFunc {
 
 func CreateFood() gin.HandlerFunc {
 	return func(c *gin.Context) {
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+		// foodId := c.Param("food_id")
+		var menu models.Menu
+		var food models.Food
 
+		if err := c.BindJSON(&food); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		}
+		validationErr := validate.Struct(food) // validate if data correct or not
+
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		err := menuCollection.FindOne(ctx, bson.M{"menu_id": *food.Menu_id}).Decode(&menu)
+
+		if err != nil {
+			msg := fmt.Sprintf("menu not found")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+
+		food.Created_at, _ = time.Parse(time.RFC3339, time.Now()).Format(time.RFC3339)
+		food.Updated_at, _ = time.Parse(time.RFC3339, time.Now()).Format(time.RFC3339)
+		food.ID = primitive.NewObjectID()
+		food.Food_id = food.ID.Hex()
+		num := toFixed(*food.Price, 2)
+		food.Price = &num
+
+		result, insertErr := foodcollection.InsertOne(ctx, food)
+		if insertErr != nil {
+			msg := fmt.Sprintf("food not created")
+			c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+			return
+		}
+		defer cancel()
+		c.JSON(http.StatusOK, result)
 	}
 }
 
--- controllers/menuController.go
+++ controllers/menuController.go
@@ -1,10 +1,37 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"context"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joy-adhikary/Restaurant-Management-back_end/database"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var menuCollection *mongo.Collection = database.OpenCollection(database.Client, "menu")
 
 func GetMenus() gin.HandlerFunc {
 
 	return func(c *gin.Context) {
+
+		var ctx, cancle = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancle()
+
+		result, err := menuCollection.Find(context.TODO(), bson.M{})
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "error occure when data fatch"})
+		}
+
+		var allMenus []bson.M
+
+		if err = result.All(ctx, &allMenus); err != nil {
+			log.Fatal(err)
+		}
+		c.JSON(http.StatusOK, allMenus)
 
 	}
 }
